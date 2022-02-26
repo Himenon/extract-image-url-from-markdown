f55ff24e0c8e099ec9158a18a73d4406a50db69b
Himenon <k.himeno314@gmail.com>
2022-02-27 00:12:50 +0900

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+import * as path from "path";
+import Glob from "glob";
+
+const getMarkdownFiles = (baseDir: string): string[] => {
+  const basePattern = path.join(baseDir, "**/*.md");
+  const files = Glob.sync(basePattern);
+  return files;
+}
+
+
+const DEBUG_DIR = "sample";
+
 const main = () => {
-  console.log("hello");
+
+  const files = getMarkdownFiles(DEBUG_DIR);
+  console.log(files);
 }
 
 main();
